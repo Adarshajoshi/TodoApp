c68a598638c8b657e483966961bce2e2bc27ea0b
Adarsh <adarshjoshi1246@gmail.com>
2026-01-19 14:34:59 +0545

    Set up testing environment

--- test/test_todos.py
+++ test/test_todos.py
@@ -37,5 +37,5 @@ client=TestClient(app)
 
 def test_read_all_authenticated():
     response = client.get("/")
-    assert response.status_code == status.HTTP_200_OK
+    assert response.status_code == 200
     assert response.json()==[]
